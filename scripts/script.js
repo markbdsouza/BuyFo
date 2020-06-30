28bc1e31a438372f75b7b6c8361869ff6a1160dd
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-06-30 23:55:57 +0530

    glider changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-// https://nickpiscitelli.github.io/Glider.js/
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+
+    dots: true,
+    infinite: true,
+    adaptiveHeight: false,
+  });
+});
